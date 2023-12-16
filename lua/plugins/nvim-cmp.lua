09e54efb8fcc860e9791700e873613c80aae8dec
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-12-16 13:57:10 +1100

    Improve shortcuts

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -25,19 +25,17 @@ return {
 			end,
 		},
 		mapping = {
-			['<C-p>'] = cmp.mapping.select_prev_item(),
-			['<C-n>'] = cmp.mapping.select_next_item(),
 			-- Add tab support
 			['<S-Tab>'] = cmp.mapping.select_prev_item(),
 			['<Tab>'] = cmp.mapping.select_next_item(),
 			['<C-d>'] = cmp.mapping.scroll_docs(-4),
 			['<C-f>'] = cmp.mapping.scroll_docs(4),
-			['<C-Space>'] = cmp.mapping.complete(),
+			-- ['<C-Space>'] = cmp.mapping.complete(),
 			['<C-e>'] = cmp.mapping.close(),
-			['<CR>'] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Insert,
-				select = true,
-			}),
+			-- ['<Tab>'] = cmp.mapping.confirm({
+			-- 	behavior = cmp.ConfirmBehavior.Insert,
+			-- 	select = false,
+			-- }),
 		},
 		-- Installed sources
 		sources = {
