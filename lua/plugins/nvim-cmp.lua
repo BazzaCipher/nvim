59d58df764f1466cda1ac311abd6d2db1c0bb82c
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-12-25 23:06:21 +1100

    Improve autocomplete shortcuts

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -26,8 +26,8 @@ return {
 		},
 		mapping = {
 			-- Add tab support
-			['<S-Tab>'] = cmp.mapping.select_prev_item(),
-			['<Tab>'] = cmp.mapping.select_next_item(),
+			['<S-Tab>'] = cmp.mapping.select_next_item(),
+			-- ['<Tab>'] = cmp.mapping.complete(),
 			['<C-d>'] = cmp.mapping.scroll_docs(-4),
 			['<C-f>'] = cmp.mapping.scroll_docs(4),
 			-- ['<C-Space>'] = cmp.mapping.complete(),
@@ -35,7 +35,19 @@ return {
 			-- ['<Tab>'] = cmp.mapping.confirm({
 			-- 	behavior = cmp.ConfirmBehavior.Insert,
 			-- 	select = false,
-			-- }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+			-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+				if cmp.visible() then
+					local entry = cmp.get_selected_entry()
+			if not entry then
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			else
+				cmp.confirm()
+			end
+				else
+					fallback()
+				end
+			end, {"i","s","c",}),			-- }),
 		},
 		-- Installed sources
 		sources = {
