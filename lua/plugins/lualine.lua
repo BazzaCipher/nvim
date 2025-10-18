2e95e319934dc6960e4a8b45ba09d81e4297b288
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2025-10-18 23:24:39 +1100

    Full path on click of relative path on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,5 +2,24 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = { theme = 'nord' }
+	opts = {
+		theme = 'nord',
+		sections = {
+			lualine_c = {
+				{
+					'filename',
+					path = 1,                 -- relative in the bar
+					shorting_target = 40,
+					file_status = true,
+					on_click = function(_, mouse_btn)
+						if mouse_btn ~= 'l' then return end
+						local p = vim.fn.expand('%:p')
+						if p == '' then return end
+						vim.notify(p, vim.log.levels.INFO, { title = 'Full path', timeout = 3000 })
+					end,
+				},
+			},
+		},
+	},
+	extensions = { 'fzf', 'nvim-tree' },
 }
